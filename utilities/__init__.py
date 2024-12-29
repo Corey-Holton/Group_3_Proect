2dc71f9b40da40be46d0a6f69f91fe4ff2cc9109
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2024-12-28 23:27:10 -0600

    Improved lyric extraction, added language list to lyric translation

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -3,4 +3,4 @@ from .separate_audio import separate_audio
 from .audio_to_midi import audio_to_midi
 from .modify_midi_main import modify_midi_prompt
 from .modify_midi_prompt_config import execute_query
-from .extract_lyrics import extract_audio_lyrics, translate_lyrics
+from .extract_lyrics import extract_lyrics, translate_lyrics, get_available_languages
